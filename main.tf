ce2549a3ed23f816cd096dc6696658c741ca066d
zYusei <102957048+zYusei@users.noreply.github.com>
2024-04-30 19:45:58 +0200

    Update main.tf

--- main.tf
+++ main.tf
@@ -25,7 +25,7 @@ resource "aws_security_group" "devOps-sg" {
   }
 }
 resource "aws_instance" "Jenkins" {
-  ami                    = "ami-0c7217cdde317cfec"  #change Ami if you different region
+  ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   key_name               = "myKeyPairDevOps"
   vpc_security_group_ids = [aws_security_group.devOps-sg.id]
@@ -39,7 +39,7 @@ resource "aws_instance" "Jenkins" {
 }
 
 resource "aws_instance" "Prometheus-Grafana" {
-  ami                    = "ami-0c7217cdde317cfec"  #change Ami if you different region
+  ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   key_name               = "myKeyPairDevOps"
   vpc_security_group_ids = [aws_security_group.devOps-sg.id]
